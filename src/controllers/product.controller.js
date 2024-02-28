af65c2c8ff800645953330492e0d2eb52ae2ae49
cengizxturkes <turkes2214@gmail.com>
2024-02-28 20:23:30 +0300

    Add getProductsController and update routes

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,19 +2,14 @@ const bcrypt = require("bcrypt");
 const APIError = require("../utils/errors");
 const Response = require("../utils/response");
 const addProductModel = require("../models/product.model");
+const getProductModel = require("../models/product.model");
 const addProductController = async (req, res) => {
   var product = addProductModel(req.body);
   product = await product.save();
   return new Response(product, "Ürün Başarıyla Eklendi").created(res);
 };
 const getProductsController = async (req, res) => {
-  try {
-    const products = await Product.find();
-    return new Response(products).success(res);
-  } catch (error) {
-    // Handle error, send appropriate response
-    console.error(error);
-    return new APIError("Error fetching products", 500).send(res);
-  }
+  const products = await getProductModel.find();
+  return new Response(products).success(res);
 };
 module.exports = { addProductController, getProductsController };
--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -8,7 +8,10 @@ const {
   resetPassword,
 } = require("../controllers/auth.controller");
 
-const { addProductController } = require("../controllers/product.controller");
+const {
+  addProductController,
+  getProductsController,
+} = require("../controllers/product.controller");
 const authValidation = require("../middlewares/validations/auth.validation");
 const { tokenCheck } = require("../middlewares/auth");
 
@@ -23,5 +26,6 @@ router.post("/forget-password", forgetPassword);
 router.post("/reset-code-check", resetCodeCheck);
 
 router.post("/reset-password", resetPassword);
-router.post("/addProduct", addProductController);
+router.post("/addProduct", tokenCheck, addProductController);
+router.get("/getProducts", tokenCheck, getProductsController);
 module.exports = router;
